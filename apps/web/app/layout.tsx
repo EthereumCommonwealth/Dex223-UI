25edd7e1ceed526bc82b34e7341c4a3a439ee285
manishnaruka <manishnaruka123@gmail.com>
2026-01-23 02:01:15 +0530

    Fix import path for UI styles in layout component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import "../assets/styles/globals.css";
-import "@repo/ui/styles.css";
+import "../../../packages/ui/src/styles.css";
 
 import clsx from "clsx";
 import { Golos_Text } from "next/font/google";
